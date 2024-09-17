596c33dbc5da42017e5e32e322e5710855caf871
Vansh <vanshgilhotra8885@gmail.com>
2024-09-17 20:02:40 +0530

    Backend Completed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 import connectdb from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from './routes/application.route.js';
 
 
 dotenv.config({});
@@ -23,6 +25,8 @@ const corsOptions = {
 
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
+app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
 
 const PORT = process.env.PORT||3000;
 app.listen(PORT,()=>{
